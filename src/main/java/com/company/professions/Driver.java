b8aa5d37bfc6e1c8f2af18040b79dc1de0fa0547
Maksym <mstefanov@provectus.com>
2021-07-20 19:29:47 +0300

    changes implemented

--- src/main/java/com/company/professions/Driver.java
+++ src/main/java/com/company/professions/Driver.java
@@ -3,10 +3,7 @@ package com.company.professions;
 import java.util.Objects;
 
 public class Driver extends Person {
-    public Driver(int experience) {
-        super();
-        this.experience = experience;
-    }
+
 
     private int experience;
 
@@ -23,13 +20,14 @@ public class Driver extends Person {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
         Driver driver = (Driver) o;
         return experience == driver.experience;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(experience);
+        return Objects.hash(super.hashCode(), experience);
     }
 
     public void setExperience(int experience) {

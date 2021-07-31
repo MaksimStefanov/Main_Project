8020141f0eb84afc4eb9d2652a9f3379e9c3bc88
Maksym <mstefanov@provectus.com>
2021-08-01 00:27:16 +0300

    Changed files added

---
+++ src/main/java/collections/SetCat3.java
@@ -0,0 +1,14 @@
+package collections;
+
+public class SetCat3 extends SetPet {
+    String name;
+
+    @Override
+    public String toString() {
+         return  name ;
+    }
+
+    public SetCat3(String name) {
+        this.name = name;
+    }
+}

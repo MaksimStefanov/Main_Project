731feb6a6ed78755eaa7040d6ad21a7362393893
MaksimStefanov <maks.stefanov13@gmail.com>
2022-03-16 16:25:32 +0200

    new fix

--- src/main/java/selfControl/main.java
+++ src/main/java/selfControl/main.java
@@ -3,6 +3,7 @@ package selfControl;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 
 public class main {
     public static void main(String[] args) throws IOException {
@@ -11,16 +12,49 @@ public class main {
         String electricity = reader.readLine();
         System.out.println("Gas: ");
         String gas = reader.readLine();
+        System.out.println("Gas Delivery: ");
+        String gasDelivery = reader.readLine();
         System.out.println("Water(11126): ");
         String water11126 = reader.readLine();
         System.out.println("Water(100832): ");
         String water100832 = reader.readLine();
-        int e = Integer.parseInt(electricity);
-        int g = Integer.parseInt(gas);
-        int w1 = Integer.parseInt(water11126);
-        int w2 = Integer.parseInt(water100832);
-        int sum = e + g + w1 + w2;
-        System.out.println(sum);
+        System.out.println("Общая сумма: " + sum(Double.parseDouble(electricity), Double.parseDouble(gas), Double.parseDouble(gasDelivery),
+                Double.parseDouble(water11126), Double.parseDouble(water100832)) + " UAH");
+        System.out.println("Максим: " + mineInvoice(Double.parseDouble(electricity), Double.parseDouble(gas), Double.parseDouble(gasDelivery),
+                Double.parseDouble(water11126), Double.parseDouble(water100832)) + " UAH");
+        System.out.println("Артем: " + artemsInvoice(Double.parseDouble(electricity), Double.parseDouble(gas), Double.parseDouble(gasDelivery),
+                Double.parseDouble(water11126), Double.parseDouble(water100832)) + " UAH");
+        System.out.println("Юля: " + juliasInvoice(Double.parseDouble(electricity), Double.parseDouble(gas), Double.parseDouble(gasDelivery),
+                Double.parseDouble(water11126), Double.parseDouble(water100832)) + " UAH");
+        System.out.println("Витя: " + vityasInvoice(Double.parseDouble(electricity), Double.parseDouble(gas), Double.parseDouble(gasDelivery),
+                Double.parseDouble(water11126), Double.parseDouble(water100832)) + " UAH");
+
     }
+
+    public static double sum(double e, double g1, double g2, double w1, double w2) {
+        double sum = e + g1 + g2 + w1 + w2;
+        return sum;
+    }
+
+    public static double mineInvoice(double e, double g1, double g2, double w1, double w2) {
+        double sum = e / 4 + g1 / 4 + g2 / 4 + w1 / 4 + w2 / 2;
+        return sum;
+    }
+
+    public static double artemsInvoice(double e, double g1, double g2, double w1, double w2) {
+        double sum = e / 4 + g1 / 4 + g2 / 4 ;
+        return sum;
+    }
+
+    public static double juliasInvoice(double e, double g1, double g2, double w1, double w2) {
+        double sum = e / 4 + g1 / 4 + g2 / 4 + w1 / 8 * 5 + w2 / 4;
+        return sum;
+    }
+
+    public static double vityasInvoice(double e, double g1, double g2, double w1, double w2) {
+        double sum = e / 4 + g1 / 4 + g2 / 4 + w1 / 8 + w2 / 4;
+        return sum;
+    }
+
 }
 

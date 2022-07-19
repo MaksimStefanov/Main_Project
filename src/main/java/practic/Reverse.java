8b46199a069a4c7f25419add1dde4a206161b5b2
MaksimStefanov <maks.stefanov13@gmail.com>
2022-07-19 15:08:44 +0300

    add new method

--- src/main/java/practic/Reverse.java
+++ src/main/java/practic/Reverse.java
@@ -1,30 +1,12 @@
 package practic;
 
 public class Reverse {
-   /* public static void main(String []args){
-        String line = "aaabccdddce";
-        System.out.println( line.chars().distinct().count() );
-    }*/
-
-    /*public static void main(String []args) {
-        String line = "организация объединённых наций";
-        char[] chars = line.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            if (i == 0 || chars[i - 1] == ' ') {
-                chars[i] = Character.toUpperCase(chars[i]);
-            }
-        }
-        System.out.println(new String(chars));*/
-
-   /*public static void main(String []args) {
-       String s = "hello";
-       char c = s.charAt(2);
-       System.out.println(c);*/
 
     public static void main(String[] args) {
         System.out.println(reverse("123_sihTekaT_123",4,12)); // переворот обрезать
         System.out.println(reverseNum(123456789)); //перевернуть число
         System.out.println(reverseStr("reverse")); //перевернуть строку
+        System.out.println(countOfLitters("wordword",false));
     }
 
     private static String reverse(String str,int start, int end) {
@@ -47,15 +29,18 @@ public class Reverse {
         }
         Integer s2= Integer.valueOf(s1);
         return s2;}
+
     private static String reverseStr(String word) {
         String str = String.valueOf(word);
         String s1 = "";
         char[] chars = str.toCharArray();
         for (int i = 0; i < str.length(); i++) {
             s1 = chars[i] + s1;
-        }
-        //Integer s2= Integer.valueOf(s1);
-        return s1;}
+        }    return s1;}
 
-
-}
+    private static Long countOfLitters(String word,boolean unique) {
+        long a = 0;
+        if (unique == false) {a = word.chars().count(); return a;}
+        if (unique == true) {a = word.chars().distinct().count(); return a;}
+        return a;
+    }}

b5404247dd2fb813d51d995c6c83c01831c2a1be
MaksimStefanov <maks.stefanov13@gmail.com>
2022-07-19 22:29:36 +0300

    refactor

--- src/main/java/practic/Reverse.java
+++ src/main/java/practic/Reverse.java
@@ -3,13 +3,13 @@ package practic;
 public class Reverse {
 
     public static void main(String[] args) {
-        System.out.println(reverse("123_sihTekaT_123",4,12)); // переворот обрезать
+        System.out.println(reverse("123_sihTekaT_123", 4, 12)); // переворот обрезать
         System.out.println(reverseNum(123456789)); //перевернуть число
         System.out.println(reverseStr("reverse")); //перевернуть строку
-        System.out.println(countOfLitters("wordword",false));
+        System.out.println(countOfLitters("wordword", false)); //сколько всех букв /сколько уникальных
     }
 
-    private static String reverse(String str,int start, int end) {
+    private static String reverse(String str, int start, int end) {
         String s1 = "";
         char[] chars = str.toCharArray();
         for (int i = 0; i < str.length(); i++) {
@@ -27,8 +27,9 @@ public class Reverse {
         for (int i = 0; i < str.length(); i++) {
             s1 = chars[i] + s1;
         }
-        Integer s2= Integer.valueOf(s1);
-        return s2;}
+        Integer s2 = Integer.valueOf(s1);
+        return s2;
+    }
 
     private static String reverseStr(String word) {
         String str = String.valueOf(word);
@@ -36,11 +37,21 @@ public class Reverse {
         char[] chars = str.toCharArray();
         for (int i = 0; i < str.length(); i++) {
             s1 = chars[i] + s1;
-        }    return s1;}
+        }
+        return s1;
+    }
 
-    private static Long countOfLitters(String word,boolean unique) {
+    private static Long countOfLitters(String word, boolean unique) {
         long a = 0;
-        if (unique == false) {a = word.chars().count(); return a;}
-        if (unique == true) {a = word.chars().distinct().count(); return a;}
+        if (unique == false) {
+            a = word.chars().count();
+            return a;
+        }
+        if (unique == true) {
+            a = word.chars().distinct().count();
+            return a;
+        }
         return a;
-    }}
+    }
+
+}
